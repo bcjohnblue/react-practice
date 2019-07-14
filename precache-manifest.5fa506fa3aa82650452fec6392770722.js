f85946c95ad69c4affb1d46f656b536549b7a14d
bcjohn <bcjohnblue@gmail.com>
2019-07-14 23:18:38 +0800

    Updates

---
+++ precache-manifest.5fa506fa3aa82650452fec6392770722.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e93b67210431124f91cbea26aba5a64a",
+    "url": "/F2E_2nd/static/media/play.e93b6721.svg"
+  },
+  {
+    "revision": "b3da108294bac552009ce36bc4757558",
+    "url": "/F2E_2nd/static/media/music.b3da1082.svg"
+  },
+  {
+    "revision": "cbd8b5a8a02328308f76296212fe3d72",
+    "url": "/F2E_2nd/static/media/list.cbd8b5a8.svg"
+  },
+  {
+    "revision": "8a5c020c4981e9cec395d7d7f01a6a47",
+    "url": "/F2E_2nd/static/media/assessment.8a5c020c.svg"
+  },
+  {
+    "revision": "e711e8ad96a2bc859e91",
+    "url": "/F2E_2nd/static/js/runtime~main.ee10c60e.js"
+  },
+  {
+    "revision": "87992bdcdced1d1c3678",
+    "url": "/F2E_2nd/static/js/main.bb8efc01.chunk.js"
+  },
+  {
+    "revision": "b351fa83ca805a68ecfe",
+    "url": "/F2E_2nd/static/js/2.c43d5c5e.chunk.js"
+  },
+  {
+    "revision": "87992bdcdced1d1c3678",
+    "url": "/F2E_2nd/static/css/main.fd015cd9.chunk.css"
+  },
+  {
+    "revision": "a5dede3e07da762d33a0690f6668d2af",
+    "url": "/F2E_2nd/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/F2E_2nd/precache-manifest.5fa506fa3aa82650452fec6392770722.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/F2E_2nd/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
