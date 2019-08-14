c00b640f6bcfc7702fac6f9283b9cf5a3e6fcd49
bcjohn <bcjohnblue@gmail.com>
2019-08-14 17:05:33 +0800

    wip

--- src/projects/Game/components/HanGuoYu/HanGuoYu.jsx
+++ src/projects/Game/components/HanGuoYu/HanGuoYu.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import styles from './HanGuoYu.module.sass';
 import clsx from 'clsx';
 
-import { ReactComponent as Role } from '../../assets/韓導1.svg';
+import RoleFront from '../../assets/韓導1.svg';
+import RoleBack from '../../assets/韓導2.svg';
 
 const HanGuoYu = props => {
   const { rolePosition } = props;
-  // const className = {
-  //   hanGuoYu:
-  // }
+
+  const roleImageList = [RoleFront, RoleBack]
+  const [roleImage, setRoleImage] = useState(RoleFront)
+  const style = {
+    role: {
+      backgroundImage: `url(${roleImage})`,
+      backgroundRepeat: 'no-repeat'
+    }
+  }
+
+  // const timerRef = useRef()
+  // useEffect(() => {
+  //   clearInterval(timerRef.current)
+  //   timerRef.current = setInterval(() => {
+  //     const newImage = roleImageList.find(img => img !== roleImage)
+  //     console.log(roleImageList, roleImage);
+  //     setRoleImage(newImage)
+  //     // style.role.backgroundImage = 
+  //   }, 100);
+  // }, [roleImage])
+  
 
   return (
     <div className={styles.han_guo_yu}>
-      <Role className={clsx(styles.role, styles[rolePosition])} />
+      <div className={clsx(styles.role, styles[rolePosition])} style={style.role}/>
     </div>
   );
 };
