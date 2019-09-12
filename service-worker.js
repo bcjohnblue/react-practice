56ed5fc034026d16c27ca87fb2ab46e8cd04f523
bcjohn <bcjohnblue@gmail.com>
2019-09-12 18:47:52 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/F2E_2nd/precache-manifest.16f955df80bf2f0691a245ef1fd9bbb5.js"
+  "/F2E_2nd/precache-manifest.951586ba4ee178ec81026c93a884e047.js"
 );
 
 workbox.clientsClaim();
